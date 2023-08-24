0d07ba4a62c456449856a8afe394b1c84c559096
veryReallyGoodCoder <6000176@sae.edu.za>
2023-08-24 10:40:48 +0200

    Sample Scene || P2 Created, Player Spawn, Movement

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -10,7 +10,7 @@ public class MovementScript : MonoBehaviour
     [SerializeField] private float mSpeed = 5f;
     private Vector2 playerInput;
 
-    private void PlayerMove(InputAction.CallbackContext ctx)
+    public void PlayerMove(InputAction.CallbackContext ctx)
     {
         playerInput = ctx.ReadValue<Vector2>();
     }
@@ -25,7 +25,7 @@ public class MovementScript : MonoBehaviour
     void Update()
     {
 
-        Vector3 move = new Vector3(playerInput.x, 0, playerInput.y);
+        Vector3 move = new Vector3(playerInput.y, 0, playerInput.x * -1);
         controller.Move(move * Time.deltaTime * mSpeed);
 
     }
---
+++ Assets/Scripts/SpawnPlayers.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpawnPlayers : MonoBehaviour
+{
+
+    public GameObject playerOnePrefab;
+    public GameObject playerTwoPrefab;
+    
+    public void SpawnPlayerOne()
+    {
+        PlayerInput.Instantiate(prefab: playerOnePrefab, playerIndex: 0, controlScheme: "Player1", pairWithDevice: Keyboard.current, splitScreenIndex: 0);
+    }
+
+    public void SpawnPlayerTwo()
+    {
+        PlayerInput.Instantiate(prefab: playerTwoPrefab, playerIndex: 1, controlScheme: "Player2", pairWithDevice: Keyboard.current, splitScreenIndex: 1);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnPlayerOne();
+        SpawnPlayerTwo();
+    }
+
+}
