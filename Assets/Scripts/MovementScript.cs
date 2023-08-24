cb4ec8e56c0f47abc07196a67e7cbccd5b86563c
veryReallyGoodCoder <6000176@sae.edu.za>
2023-08-24 10:09:50 +0200

    Player Movement || Script Created

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class MovementScript : MonoBehaviour
+{
+
+    private CharacterController controller;
+    [SerializeField] private float mSpeed = 5f;
+    private Vector2 playerInput;
+
+    private void PlayerMove(InputAction.CallbackContext ctx)
+    {
+        playerInput = ctx.ReadValue<Vector2>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 move = new Vector3(playerInput.x, 0, playerInput.y);
+        controller.Move(move * Time.deltaTime * mSpeed);
+
+    }
+}
