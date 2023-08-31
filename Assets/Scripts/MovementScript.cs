9d69d41ceb13a11f9c3dbe44bc16c601aeeaa997
veryReallyGoodCoder <6000176@sae.edu.za>
2023-08-31 10:34:56 +0200

    Something Scene || Jump Added

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -2,17 +2,34 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
+//[RequireComponent(typeof(Rigidbody))]
+
 
 public class MovementScript : MonoBehaviour
 {
 
     private CharacterController controller;
+
     [SerializeField] private float mSpeed = 5f;
+
     private Vector2 playerInput;
+
+    private bool jump;
+
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField] private float gravityValue = -9.81f;
 
     public void PlayerMove(InputAction.CallbackContext ctx)
     {
         playerInput = ctx.ReadValue<Vector2>();
+    }
+
+    public void PlayerJump(InputAction.CallbackContext ctx)
+    {
+        jump = ctx.action.triggered;
+        Debug.Log(jump);
     }
 
     // Start is called before the first frame update
@@ -24,9 +41,26 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+         groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
 
-        Vector3 move = new Vector3(playerInput.x, 0, playerInput.y);
+
+        Vector3 move = new Vector3(playerInput.x, playerVelocity.y, playerInput.y);
         controller.Move(move * Time.deltaTime * mSpeed);
+
+
+       
+        if (jump && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
 
     }
 }
+    
